c049381b4b8c6a9fbac797051f1e48caa060fe04
王科翔 <kx.wang@hotmail.com>
2018-09-28 01:49:33 +0800

    ?

---
+++ task0/cmake-build-debug/CMakeFiles/task0.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dywkx/CLionProjects/course_of_compiler_theory/task0/main.cpp" "/home/dywkx/CLionProjects/course_of_compiler_theory/task0/cmake-build-debug/CMakeFiles/task0.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ task0/cmake-build-debug/CMakeFiles/task0.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task0.dir/main.cpp.o"
+  "task0.pdb"
+  "task0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
